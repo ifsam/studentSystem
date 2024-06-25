b7a66f84293c5d834cb5acf8e3a3c11f1315c269
qinwei <598521237@qq.com>
2024-06-25 22:42:00 +0800

    test qinwei

--- src/com/qin/services/impl/StuServiceImpl.java
+++ src/com/qin/services/impl/StuServiceImpl.java
@@ -27,6 +27,7 @@ public class StuServiceImpl implements StuService {
 
     @Override
     public List<Student> queryStuBy(Student student, Page page) {
+        System.out.println(222);
         return stuDao.queryStuBy(student,page);
     }
 }
